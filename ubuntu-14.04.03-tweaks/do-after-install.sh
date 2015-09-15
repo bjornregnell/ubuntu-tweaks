3f43966604ab0427311d2410001b3952633187ab
Bjorn Regnell <bjorn.regnell@cs.lth.se>
2015-09-15 17:49:52 +0200

    fix comment

--- ubuntu-14.04.03-tweaks/do-after-install.sh
+++ ubuntu-14.04.03-tweaks/do-after-install.sh
@@ -30,12 +30,13 @@ sudo apt-get install git
 sudo mkdir /usr/lib/jvm
 sudo mv ~/Downloads/jdk1.8.0_60 /usr/lib/jvm/oracle_jdk8
 gksudo gedit /etc/profile.d/oraclejdk.sh
-#paste code below until ##
+# to set vital paths paste the code below until ## in the file open in gedit 
 export J2SDKDIR=/usr/lib/jvm/oracle_jdk8
 export J2REDIR=/usr/lib/jvm/oracle_jdk8/jre
 export PATH=$PATH:/usr/lib/jvm/oracle_jdk8/bin:/usr/lib/jvm/oracle_jdk8/db/bin:/usr/lib/jvm/oracle_jdk8/jre/bin
 export JAVA_HOME=/usr/lib/jvm/oracle_jdk8
 export DERBY_HOME=/usr/lib/jvm/oracle_jdk8/db
+##
 
 # *** eclipse
 ## http://www.eclipse.org/downloads/
