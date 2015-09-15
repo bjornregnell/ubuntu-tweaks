4a8f86da604fd6455dd5163ee9d1d7ff2f7c89c2
Bjorn Regnell <bjorn.regnell@cs.lth.se>
2015-09-15 17:42:00 +0200

    fix comments

--- ubuntu-14.04.03-tweaks/do-after-install.sh
+++ ubuntu-14.04.03-tweaks/do-after-install.sh
@@ -15,7 +15,6 @@ sudo update-grub
 # Get gksudo to enable sudo with window apps:
 sudo apt-get install gksu
 
-# ************** developer tools
 # *** git
 # http://stackoverflow.com/questions/19109542/installing-latest-version-of-git-in-ubuntu
 sudo add-apt-repository ppa:git-core/ppa
@@ -68,20 +67,13 @@ ls /opt/sublime_text/Packages/
 cd /opt/sublime_text/Packages/
 sudo cp ~/Downloads/Language\ -\ English.sublime-package .
 
-
-# ************** other applications
-
 # *** Latex
 # https://help.ubuntu.com/community/LaTeX
 sudo apt-get install texlive-full
 sudo apt-get install texlive-lang-swedish
-sudo apt-get install texworks  #some syntax highlighting problems at first 
-# this is old 4.4 sudo apt-get install texmaker
-# get latest deb package here:
-# http://www.xm1math.net/texmaker/download.html#linux
-# download and install from above
+sudo apt-get install texworks  
 apt-get install myspell-sv-se
-# add custom latex .sty files from lth here:
+# put your custom latex .sty files from lth here:
 # http://tex.stackexchange.com/questions/1137/where-do-i-place-my-own-sty-or-cls-files-to-make-them-available-to-all-my-te
 
 # *** ms fonts
@@ -100,23 +92,10 @@ sudo dpkg -i pandoc-1.15.0.6-1-amd64.deb
 #  to enable putting things in the paste buffer, e.g. xclip -sel clip < ~/.ssh/id_rsa.pub
 sudo apt-get install xclip
 
-
 # Fix menu in window title bar
 #http://askubuntu.com/questions/541449/14-04-always-show-menu-items
-
 # Aargh on tab-switcher. Hope for solution in coming LTS... Learn how to use super-W instead; probably quicker...
 # http://askubuntu.com/questions/68151/how-do-i-revert-alt-tab-behavior-to-switch-between-windows-on-the-current-worksp/68171#68171
 # http://ubuntuforums.org/showthread.php?t=2211863
 # https://bugs.launchpad.net/ubuntu/+source/compiz/+bug/1282900
 # Compizconfig is unfortunately shaky and dangerous (I tried below but the static swithcer crashed frequently)
-# sudo apt-get install compizconfig-settings-manager
-# sudo apt-get install compiz-plugins
-# Open compizconfig-settings-manager with alt-F2, type ccsm.
-#   Scroll down to "Ubuntu Unity Plugin". Choose the tab "Switcher". 
-#   Disable the alt-tab and shift-alt-tab key bindings. ("Key to start the switcher" and "Key to switch to the previous window in the Switcher".
-#   Click the "Back" button.
-#   Scroll down to the "Window management" section. Here you can select another switcher. 
-#   I enable the "Static Application Switcher", resolve any potential conflicts by setting the setting for "Static Application Switcher".
-#   Now you can tweak the switcher by clicking on it. I have changed alt-tab and shift-alt-tab to "Next window (All windows)" and "Prev window (All windows)".
-
-
